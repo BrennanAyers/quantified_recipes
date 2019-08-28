b16bb5a1d624b5269fc86dec967d1766108599cb
Trevor Nodland <trevor.nodland@gmail.com>
2019-08-28 09:55:48 -0600

    Adds initial migration for recipe resource in database
    Co-authored-by: Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>

---
+++ models/recipe.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Recipe = sequelize.define('Recipe', {
+    foodType: DataTypes.STRING,
+    recipeUrl: DataTypes.STRING,
+    recipeImage: DataTypes.STRING,
+    ingredientList: DataTypes.TEXT,
+    ingredientCount: DataTypes.INTEGER,
+    calorieCount: DataTypes.INTEGER,
+    servingCount: DataTypes.INTEGER
+  }, {});
+  Recipe.associate = function(models) {
+    // associations can be defined here
+  };
+  return Recipe;
+};
