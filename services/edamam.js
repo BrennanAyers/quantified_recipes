4058915928cf902f77e4855ca8ea888bd17fa022
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-08-28 17:51:33 -0600

    Fix indentation on foodType Edamam Service function, remove extra spacing
    Co-Authored-By: Trevor Nodland <42710030+tnodland@users.noreply.github.com>

--- services/edamam.js
+++ services/edamam.js
@@ -3,27 +3,27 @@ const fetch = require('node-fetch')
 
 class Edamam {
   foodType(query) {
-   return fetch(`https://api.edamam.com/search?q=${query}&app_id=${process.env.EDAMAM_API_ID}&app_key=${process.env.EDAMAM_API_KEY}&to=3&health=alcohol-free`)
-   .then(response => {
-     return response.json()
-     .then(json => {
-       let recipes = []
-       json.hits.forEach(function(recipe) {
-         recipes.push({
-           name: recipe.recipe.label,
-           foodType: query,
-           recipeUrl: recipe.recipe.url,
-           recipeImage: recipe.recipe.image,
-           ingredientList: recipe.recipe.ingredientLines.join(','),
-           ingredientCount: recipe.recipe.ingredientLines.length,
-           calorieCount: recipe.recipe.calories,
-           servingCount: recipe.recipe.yield
-         })
-       })
-       return recipes
-     })
-   })
- }
+    return fetch(`https://api.edamam.com/search?q=${query}&app_id=${process.env.EDAMAM_API_ID}&app_key=${process.env.EDAMAM_API_KEY}&to=3&health=alcohol-free`)
+    .then(response => {
+      return response.json()
+      .then(json => {
+        let recipes = []
+        json.hits.forEach(function(recipe) {
+          recipes.push({
+            name: recipe.recipe.label,
+            foodType: query,
+            recipeUrl: recipe.recipe.url,
+            recipeImage: recipe.recipe.image,
+            ingredientList: recipe.recipe.ingredientLines.join(','),
+            ingredientCount: recipe.recipe.ingredientLines.length,
+            calorieCount: recipe.recipe.calories,
+            servingCount: recipe.recipe.yield
+          })
+        })
+        return recipes
+      })
+    })
+  }
 
   ingredientCount(query, num_of_ingredients) {
     return fetch(`https://api.edamam.com/search?q=${query}&ingr=${num_of_ingredients}&app_id=${process.env.EDAMAM_API_ID}&app_key=${process.env.EDAMAM_API_KEY}&to=3&health=alcohol-free`)
@@ -48,8 +48,6 @@ class Edamam {
     })
   }
 }
-
-
 
 module.exports = {
   Edamam: Edamam
