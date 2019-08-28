4d5d7b4de589e7a8052efd343b2d6f5b99ea22e3
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-08-28 13:45:26 -0600

    Add initial test for Edamam API Service foodType function
    Co-Authored-By: Trevor Nodland <42710030+tnodland@users.noreply.github.com>

--- test/services/edamam.spec.js
+++ test/services/edamam.spec.js
@@ -3,9 +3,27 @@ var request = require("supertest");
 var app = require('../../app');
 var Edamam = require('../../services/edamam').Edamam
 var edamamService = new Edamam
+var Recipe = require('../../models').Recipe
 
 describe('Edamam API Service Tests', () => {
   test('It is an instance of Edamam', () => {
     expect(edamamService).toBeInstanceOf(Edamam)
+  })
+
+  test('It can get recipes by food type', () => {
+    return edamamService.foodType('chicken')
+    .then(recipes => {
+      expect(recipes.length).toBe(3)
+      expect(Object.keys(recipes[0])).toContain('name')
+      expect(Object.keys(recipes[0])).toContain('recipeUrl')
+      expect(Object.keys(recipes[0])).toContain('recipeImage')
+      expect(Object.keys(recipes[0])).toContain('ingredientList')
+      expect(Object.keys(recipes[0])).toContain('calorieCount')
+      expect(Object.keys(recipes[0])).toContain('servingservingCount')
+      return Recipe.findAll()
+      .then(dbRecipes => {
+        expect(dbRecipes.length).toBe(3)
+      })
+    })
   })
 })
