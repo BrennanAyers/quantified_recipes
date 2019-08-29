5f0c2f0f5687e3b6c4222809093c02eebf142a52
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-08-28 19:06:05 -0600

    Add test to ensure limit of response from Recipes Ingredient Sort endpoint

--- test/ingredient_sort.spec.js
+++ test/ingredient_sort.spec.js
@@ -98,4 +98,73 @@ describe('Ingredient Count Sorted Recipes Endpoint', () => {
       })
     })
   })
+
+  test('it returns only 5 recipes in a sorted list of ingredients descending', () => {
+    return Recipe.bulkCreate([{
+      name: 'chicken dish',
+      foodType: 'chicken',
+      recipeUrl: 'www.chicken_recipe.com',
+      recipeImage: 'image.com',
+      ingredientList: '1 Chicken, 2 Lemons',
+      ingredientCount: 5,
+      calorieCount: 2000,
+      servingCount: 4
+    },
+    {
+      name: 'turkey dish',
+      foodType: 'turkey',
+      recipeUrl: 'www.turkey_recipe.com',
+      recipeImage: 'image.com',
+      ingredientList: '1 Turkey, 2 Lemons',
+      ingredientCount: 2,
+      calorieCount: 2000,
+      servingCount: 4
+    },
+    {
+      name: 'emu dish',
+      foodType: 'emu',
+      recipeUrl: 'www.emu_recipe.com',
+      recipeImage: 'image.com',
+      ingredientList: '1 Emu, 2 Lemons',
+      ingredientCount: 2,
+      calorieCount: 2000,
+      servingCount: 4
+    },
+    {
+      name: 'dodo dish',
+      foodType: 'dodo',
+      recipeUrl: 'www.dodo_recipe.com',
+      recipeImage: 'image.com',
+      ingredientList: '1 Dodo, 2 Lemons',
+      ingredientCount: 2,
+      calorieCount: 2000,
+      servingCount: 4
+    },
+    {
+      name: 'moa dish',
+      foodType: 'moa',
+      recipeUrl: 'www.moa_recipe.com',
+      recipeImage: 'image.com',
+      ingredientList: '1 Moa, 2 Lemons',
+      ingredientCount: 2,
+      calorieCount: 2000,
+      servingCount: 4
+    },
+    {
+      name: 'pheasant dish',
+      foodType: 'pheasant',
+      recipeUrl: 'www.pheasant_recipe.com',
+      recipeImage: 'image.com',
+      ingredientList: '1 Pheasant, 2 Lemons',
+      ingredientCount: 10,
+      calorieCount: 2000,
+      servingCount: 4
+    }])
+    .then(chickens => {
+      return request(app).get('/api/v1/recipes/ingredient_sort?amount=desc')
+      .then(response => {
+        expect(response.body.length).toBe(5)
+      })
+    })
+  })
 })
