86698b4e9a5ebf3dc2a989d68b3931eb2472b41c
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-08-28 19:00:59 -0600

    Add direction variable for Recipes ingredient_sort route to determine ordering by ingredientCount

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -97,8 +97,9 @@ router.get('/ingredient_search', function(req, res, next) {
 })
 
 router.get('/ingredient_sort', function(req, res, next) {
+  let direction = req.query.amount == 'asc' ? 'ASC' : 'DESC'
   Recipe.findAll({
-    order: [['ingredientCount', 'DESC']]
+    order: [['ingredientCount', direction]]
   })
   .then(recipes => {
     res.setHeader('Content-Type', 'application/json');
